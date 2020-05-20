77c1dcead8426c7c2a2e379af452b3b6e5741d16
rubenfgr87 <rubenfgr87@outlook.com>
2020-05-20 11:20:39 +0200

    Refactorización de código

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser
 server.app.use(bodyParser.urlencoded({ extended: true }));
